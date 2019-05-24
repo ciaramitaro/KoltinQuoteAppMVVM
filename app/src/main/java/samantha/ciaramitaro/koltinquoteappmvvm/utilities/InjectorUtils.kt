61aa6896fe453f9287c19025cd09703b3e06197c
ciaramitaro <s.n.ciaramitaro@gmail.com>
2019-05-23 21:24:17 -0500

    Created QuotesViewModelFactory class, put code in it, created dependency injection class

---
+++ app/src/main/java/samantha/ciaramitaro/koltinquoteappmvvm/utilities/InjectorUtils.kt
@@ -0,0 +1,6 @@
+package samantha.ciaramitaro.koltinquoteappmvvm.utilities
+
+//This singleton doesn't need to be passed anything which is why it's an object and not a class.
+object InjectorUtils {
+
+}
