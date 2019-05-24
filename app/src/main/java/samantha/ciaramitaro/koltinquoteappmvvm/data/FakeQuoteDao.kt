933fd6f3382ea60be2a3754d165cfeaafe951524
ciaramitaro <s.n.ciaramitaro@gmail.com>
2019-05-23 20:20:50 -0500

    Started Dao class structure & added UI structure

--- app/src/main/java/samantha/ciaramitaro/koltinquoteappmvvm/data/FakeQuoteDao.kt
+++ app/src/main/java/samantha/ciaramitaro/koltinquoteappmvvm/data/FakeQuoteDao.kt
@@ -1,4 +1,18 @@
 package samantha.ciaramitaro.koltinquoteappmvvm.data
 
+import androidx.lifecycle.MutableLiveData
+
 class FakeQuoteDao {
+    private val quoteList = mutableListOf<Quote>()
+    private val quotes = MutableLiveData<List<Quote>>()
+    
+    init {
+        //This connects the list to the observable data holder class
+        quotes.value = quoteList
+    }
+    fun addQuote(quote: Quote){
+        quoteList.add(quote)
+        //This updates the obersvable
+        quotes.value = quoteList
+    }
 }
--- app/src/main/java/samantha/ciaramitaro/koltinquoteappmvvm/data/Quote.kt
+++ app/src/main/java/samantha/ciaramitaro/koltinquoteappmvvm/data/Quote.kt
@@ -1,4 +1,8 @@
 package samantha.ciaramitaro.koltinquoteappmvvm.data
 
-class Quote {
+data class Quote (val quoteText: String,
+                  val author: String){
+    override fun toString(): String {
+        return "$quoteText - $author"
+    }
 }
