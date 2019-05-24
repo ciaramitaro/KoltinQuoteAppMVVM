dfe4ba412d812d2f4b39ca7f9c902c89dbaa61eb
ciaramitaro <s.n.ciaramitaro@gmail.com>
2019-05-23 20:42:05 -0500

    created FakeDatabase singleton class till I connect app to real database

--- app/src/main/java/samantha/ciaramitaro/koltinquoteappmvvm/data/FakeDatabase.kt
+++ app/src/main/java/samantha/ciaramitaro/koltinquoteappmvvm/data/FakeDatabase.kt
@@ -1,5 +1,22 @@
 package samantha.ciaramitaro.koltinquoteappmvvm.data
-
+//Creating Java style Singleton class
 class FakeDatabase private constructor(){
+
+    //holder of all objects
+    val quoteDao = FakeDatabase()
+
+
+    companion object{
+
+        //Volatile makes it accessible by other threads.
+        @Volatile private var instance: FakeDatabase? = null
+
+        fun getInstance()=
+            //If the instance is not null, it will return the current instance being checked,
+            //if it is null then we'll return a new FakeDatabase instance (not the null)
+                instance ?: synchronized(this){
+                    instance?: FakeDatabase().also { instance = it }
+                }
+    }
 
 }
